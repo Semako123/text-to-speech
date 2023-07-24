92dc889acbb2bd1cfdc15335c109328b50606011
semako123 <zosusemenoch@gmail.com>
2023-07-24 22:28:15 +0100

    Adds load function to turn texts to speech using hugging face ttc model

--- app.py
+++ app.py
@@ -6,11 +6,20 @@ load_dotenv(find_dotenv())
 HUGGING_FACE_KEY = os.getenv("HUGGING_FACE_KEY")
 story_text = ""
 
-with open('story.txt') as story:
+with open("story.txt") as story:
     story_text = story.read()
 
-print(story_text)
 
 def load_audio(texts):
-    pass
-        
+    API_URL = (
+        "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    )
+    headers = {"Authorization": "Bearer " + HUGGING_FACE_KEY}
+
+    response = requests.post(API_URL, headers=headers, json={"inputs": texts})
+
+    with open("audio.flac", "wb") as audio:
+        audio.write(response.content)
+
+
+load_audio(story_text)
